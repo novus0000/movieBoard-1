40c9fb92c48fa441302aa76b3cf104911b54ba0c
novus0000 <55471825+novus0000@users.noreply.github.com>
2019-12-10 06:09:51 +0900

    Add files via upload

--- MovieBoard/SearchMovie.swift
+++ MovieBoard/SearchMovie.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 import UIKit
+import CoreData
+
+var mymovielist:[movieList]=[]
+        
+    
+    
 
 class SearchMovie: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
@@ -16,7 +22,6 @@ class SearchMovie: UIViewController {
        var recognizer = UITapGestureRecognizer(target:self, action: #selector(dismissKeyboard))
        return recognizer
      }()
-    
     var searchResult:[movieList] = []
     let NaverAPI = naverAPI()
     
@@ -25,11 +30,8 @@ class SearchMovie: UIViewController {
     }
 
 
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         navigationController?.navigationBar.prefersLargeTitles = true
         // Do any additional setup after loading the view.
     }
@@ -59,42 +61,71 @@ extension SearchMovie: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "movieList", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieList", for: indexPath) as? SearchTableViewCell
+        cell?.cellDelegate = self
+        cell?.index = indexPath
         
         let item = searchResult[indexPath.row]
-        configureText(for: cell, with: item)
-        configureImage(for: cell, with: item)
-        return cell
-    }
-    
-    func configureText(for cell: UITableViewCell, with item: movieList) {
-        if let label1 = cell.viewWithTag(1001) as? UILabel {
-            label1.text = item.title
-        }
-        if let label2 = cell.viewWithTag(1002) as? UILabel {
-            label2.text = item.subtitle
-        }
-        if let label3 = cell.viewWithTag(1003) as? UILabel {
-            label3.text = "pubDate: " + item.pubDate
-        }
-        if let label4 = cell.viewWithTag(1004) as? UILabel {
-            label4.text = "director: " + item.director
-        }
-        if let label5 = cell.viewWithTag(1005) as? UILabel {
-            label5.text = "actor: " + item.actor
-        }
-        if let label6 = cell.viewWithTag(1006) as? UILabel {
-            label6.text = "user rating: " + item.userRating
-        }
-        if let label7 = cell.viewWithTag(1007) as? UILabel {
-            label7.text = item.link
-        }
-    }
-    
-    func configureImage(for cell: UITableViewCell, with item: movieList) {
-        if let label = cell.viewWithTag(10) as? UIImageView {
-            label.image = str2Img(imageStr: item.image)
-        }
+        
+        cell?.label1.text = item.title
+        cell?.label2.text = item.subtitle
+        cell?.label3.text = "pubDate: " + item.pubDate
+        cell?.label4.text = "director: " + item.director
+        cell?.label5.text = "actor: " + item.actor
+        cell?.label6.text = "user rating: " + item.userRating
+        cell?.label7.text = item.link
+        cell?.imagelabel.image = str2Img(imageStr: item.image)
+//        configureText(for: cell, with: item)
+//        configureImage(for: cell, with: item)
+//
+        return cell!
     }
 
+//    func configureText(for cell: UITableViewCell, with item: movieList) {
+//        if let label1 = cell.viewWithTag(1001) as? UILabel {
+//            label1.text = item.title
+//        }
+//        if let label2 = cell.viewWithTag(1002) as? UILabel {
+//            label2.text = item.subtitle
+//        }
+//        if let label3 = cell.viewWithTag(1003) as? UILabel {
+//            label3.text = "pubDate: " + item.pubDate
+//        }
+//        if let label4 = cell.viewWithTag(1004) as? UILabel {
+//            label4.text = "director: " + item.director
+//        }
+//        if let label5 = cell.viewWithTag(1005) as? UILabel {
+//            label5.text = "actor: " + item.actor
+//        }
+//        if let label6 = cell.viewWithTag(1006) as? UILabel {
+//            label6.text = "user rating: " + item.userRating
+//        }
+//        if let label7 = cell.viewWithTag(1007) as? UILabel {
+//            label7.text = item.link
+//        }
+//    }
+//
+//    func configureImage(for cell: UITableViewCell, with item: movieList) {
+//        if let label = cell.viewWithTag(10) as? UIImageView {
+//            label.image = str2Img(imageStr: item.image)
+        }
+//    }
+
+
+
+extension SearchMovie: (SearchTabelViewprotocol){
+    func onClickCell(index: Int) {
+        print(searchResult[index].title + " is clicked")
+        mymovielist.append(searchResult[index])
+//        UserDefaults.standard.set(mymovielist, forKey: "mymovielist")
+//        for i in 0...mymovielist.count-1{
+//            print(mymovielist[i].title)
+//        }
+        
+//        listname.append(searchResult[index])
+        
+//        savedlist.setProperty(_property: searchResult[index].title)
+//        print(savedlist.getproperty)
+    }
+        
 }
--- MovieBoard/ViewController.swift
+++ MovieBoard/ViewController.swift
@@ -8,24 +8,78 @@
 
 import UIKit
 
-class ChecklistViewController: UIViewController {
-    @IBOutlet weak var label: UILabel!
-    @IBAction func goSearchMovie(_ sender: UIBarButtonItem) {
+    
+class ViewController :UICollectionViewController {
+
+    var testitems = ["a","b","c"] // This is the test item
+    
+//    @IBAction func reload(_ sender: Any) {
+//        self.collectionView.reloadData()}
+// manual reload button
+    
+    @IBAction func goSearchMovie(_ sender: Any) {
         let storyBoard = UIStoryboard(name: "SearchMovie", bundle: nil)
         let viewController = storyBoard.instantiateViewController(identifier: "SearchMovie")
 
         navigationController?.pushViewController(viewController, animated: true)
-            
+//If you press + on the top of screen, SearchMovie screen will appear.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+            collectionView.reloadData()
+        // After add list and you back, reload(refresh) main page
+    }
     override func viewDidLoad() {
-        super.viewDidLoad()
         
+      
+        super.viewDidLoad()
+         self.collectionView.reloadData()
         navigationController?.navigationBar.prefersLargeTitles = true
         // Do any additional setup after loading the view.
     }
+
+        override func numberOfSections(in collectionView: UICollectionView) -> Int {
+
+            return 1
+
+        }
+
+
+
+        override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return mymovielist.count
+// number of collection cell = mymovielist items
+        }
+
+
+        override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Mycell", for: indexPath) as? CollectionViewCell
+            cell?.cellimage.image = str2Img(imageStr: mymovielist[indexPath.row].image)
+//            cell?.cellimage.image = str2Img(imageStr: mymovielist[indexPath.row].image)
+//            cell?.label1.text = mymovielist[indexPath.row].title
+
+            return cell!
+
+        
+        }
     
+    func str2Img(imageStr: String) -> UIImage? {
+        if !imageStr.isEmpty {
+            let imageUrl = URL(string: imageStr)
+            do {
+                let imageData = try Data(contentsOf: imageUrl!)
+                return UIImage(data: imageData)!
+            } catch let error as NSError {
+                print(error)
+            }
+            return nil
+        } else {
+            return nil
+        }
+    }
     
+    }
     
-    
-}
+
